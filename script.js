2457f858a34cb055c45484bb622172ec734f07cb
JuanC <castrojuanig25@gmail.com>
2025-10-21 13:04:07 -0300

    manipulacion del dom

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const boton = document.querySelector('#boton-importante');
-      boton.addEventListener('click', () => {
-        alert('¡Gracias por tu interés en este empleo! Serás redirigido a la página de aplicación.');
-      })
+//recupera solo el primer boton que se encuentre
+const boton = document.querySelector('.button-apply-job');
+
+//si encuentra mucho devuelve una lista o una lista vacia si no encuentra ninguno
+const botones = document.querySelectorAll('.button-apply-job');
+
+botones.forEach(boton => {
+    boton.addEventListener('click', () => {
+    boton.textContent = "!Aplicado";
+    boton.classList.add('is-applied');
+    boton.disabled = true;
+})
+})
