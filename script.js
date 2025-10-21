bd932d50e8ddffb3b8ca4e61cd5719007114664b
JuanC <castrojuanig25@gmail.com>
2025-10-21 12:43:56 -0300

    eventes en js

---
+++ script.js
@@ -0,0 +1,4 @@
+const boton = document.querySelector('#boton-importante');
+      boton.addEventListener('click', () => {
+        alert('¡Gracias por tu interés en este empleo! Serás redirigido a la página de aplicación.');
+      })
